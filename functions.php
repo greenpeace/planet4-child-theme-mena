67bf51d5f4313da17c1100d6dcd5ba07075b3a60
fadigedeon <fgedeon@greenpeace.org>
2025-04-11 13:09:48 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,16 +4,16 @@
  * Additional code for the child theme goes in here.
  */
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
-wp-content/themes/planet4-child-theme-mena/functions.php
-function enqueue_child_styles() {
-	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
+// Enqueue child theme styles
+add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99 );
 
+function enqueue_child_styles() {
+	$css_creation = filectime( get_stylesheet_directory() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
 
-// Use arabic analyzer when needed.
+// Use Arabic analyzer when needed.
 // Ref: https://github.com/10up/ElasticPress/issues/1132
-add_filter('ep_analyzer_language', static function (): string {
+add_filter( 'ep_analyzer_language', static function (): string {
 	return 'arabic';
-});
+} );
